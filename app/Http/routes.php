70c499fbf4150823e27839cc9e2648880a41421c
sinarahmany <sinarahmannejad@gmail.com>
2021-05-04 23:54:47 -0700

    Learning CRUD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,32 @@ Route::get('/', function () {
 Route::get('/post/{id}', function ($id) {
     return "this is post number" . $id;
 });
+
+/*
+|--------------------------------------------------------------------------
+| DATABASE RAW SQL QUERIES
+|--------------------------------------------------------------------------
+*/
+Route::get('/insert', function () {
+    DB::insert('insert into posts (title, content) values(?,?)' , ['No sleep','Cause learning Laravel']);
+});
+
+Route::get('/read', function () {
+    $results = DB::select('select * from posts where id = ?' , [1]);
+    foreach ($results as $posts){
+        return "Title: " . $posts->title . "<br> Content: " . $posts->content;
+    }
+
+});
+
+Route::get('/update', function () {
+    $updated = DB::update('update posts set title = "this is the new title but still no sleep!" where id = ?' , [1]);
+    return $updated;
+
+});
+
+Route::get('/delete', function () {
+    $deleted = DB::delete('delete from posts where id = ?' , [3]);
+    return $deleted;
+
+});
