3527b95e0245862e4dd11e590629e5f1dac96179
sinarahmany <sinarahmannejad@gmail.com>
2021-05-11 23:03:54 -0700

    using Tinker

---
+++ app/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    protected $fillable = [
+        'title',
+        'content'
+    ];
+
+    public  function user(){
+       return $this->belongsTo('App\User');
+    }
+
+    public function photos(){
+        return $this->morphMany('App\Photo','imageable');
+    }
+
+    public function tags(){
+        return $this->morphToMany('App\Tag' ,'taggable');
+    }
+}
